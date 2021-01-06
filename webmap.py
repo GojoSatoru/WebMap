6af9d385ec9bc4ef6f923c495a323a395622987a
Gojo <57420796+GojoSatoru@users.noreply.github.com>
2021-01-05 18:19:37 -0600

    condensing code ig

--- webmap.py
+++ webmap.py
@@ -1,8 +1,15 @@
 #!/usr/bin/python3
-
+#GojoSatoru, Anteste, 
 import os
 import sys
 import socket
+
+def underscore():
+	 print("______________________________________________________________________")
+
+
+def enviroment():
+	os.system('cls' if os.name == 'nt' else 'clear')
 
 ans=True
 while ans:
@@ -18,12 +25,12 @@ while ans:
     print("E. Exit")
     print('====================================')
     ans=input("What would you like to do? Enter your selection: ")
-    
+
     if ans=="1":
-      os.system('cls' if os.name == 'nt' else 'clear')
+      enviroment()
       ans=True
       while ans:
-          print
+          print()
           print('====================================')
           print('#             Nmap Scan            #')
           print('====================================')
@@ -33,94 +40,94 @@ while ans:
           print("4. Scan The Network For All Devices")
           print("M. Main Menu")
           print('====================================')
-          print 
+          print
           ans=input("What would you like to do? Enter your selection: ")
 
           if ans=="1":
-                os.system('cls' if os.name == 'nt' else 'clear')
+                enviroment()
                 print
-                print("______________________________________________________________________")
+                underscore()
                 print
                 portscan=input("Enter the IP you want to scan the ports of: ")
                 portipscan = socket.gethostbyname(portscan)
-                print("______________________________________________________________________")
+                underscore()
                 os.system("nmap " + portipscan)
-                print("______________________________________________________________________")
+                underscore()
           elif ans=="2":
-                os.system('cls' if os.name == 'nt' else 'clear')
+                enviroment()
                 print
-                print("______________________________________________________________________")
+                underscore()
                 print
                 osscan=input("Enter the IP you want to find the operating system of: ")
                 osipscan = socket.gethostbyname(osscan)
-                print("______________________________________________________________________")
+                underscore()
                 os.system("nmap -O " + osipscan)
-                print("______________________________________________________________________") 
-                  
+                underscore()
+
           elif ans=="3":
-                  os.system('cls' if os.name == 'nt' else 'clear')
+                  enviroment()
                   print
-                  print("______________________________________________________________________")
+                  underscore()
                   print
                   ascan=input("Enter the IP you want to scan: ")
                   aipscan = socket.gethostbyname(ascan)
                   print
-                  print("______________________________________________________________________")
+                  underscore()
                   os.system("nmap -A " + aipscan)
-                  print("______________________________________________________________________")
+                  underscore()
           elif ans=="4":
-                os.system('cls' if os.name == 'nt' else 'clear')
+                enviroment()
                 print
-                print("______________________________________________________________________")
+                underscore()
                 print
                 snscan=input("Enter your address and range (i.e. 192.168.0.1/24) now: ")
                 snipscan = socket.gethostbyname(snscan)
                 print
-                print("______________________________________________________________________")
+                underscore()
                 os.system("nmap -sn " + snipscan)
-                print("______________________________________________________________________")
+                underscore()
           elif ans=="M":
-                  os.system('cls' if os.name == 'nt' else 'clear')
+                  enviroment()
                   os.system('python3 webmap.py')
-                  
+
           else:
-                  os.system('cls' if os.name == 'nt' else 'clear')
+                  enviroment()
                   print
                   print("Not Valid Choice Try again")
                   print
                   os.system("python3 webmap.py")
                   ans = None
     elif ans=="2":
-      os.system('cls' if os.name == 'nt' else 'clear')
+      enviroment()
       print
       print('====================================')
       print('#        Dirsearch Scan            #')
       print('====================================')
       print
       dirtarget=input("Enter target: ")
-      print("___________________________________________________________________________")
+      underscore()
       print
       os.system("/opt/dirsearch/dirsearch.py -u " + dirtarget)
-      print("______________________________________________________________________")
+      underscore()
       if ans=="":
-          os.system('cls' if os.name == 'nt' else 'clear')
+          enviroment()
     elif ans=="3":
-      os.system('cls' if os.name == 'nt' else 'clear')
+      enviroment()
       print
       print('====================================')
       print('#            Nikto Scan            #')
       print('====================================')
       print
       niktotarget=input("Enter target: ")
-      print("___________________________________________________________________________")
+      underscore()
       print
       os.system("nikto -host " + niktotarget)
-      print("______________________________________________________________________")
+      underscore()
       if ans=="":
-          os.system('cls' if os.name == 'nt' else 'clear')
-    
+          enviroment()
+
     elif ans=="A":
-        os.system('cls' if os.name == 'nt' else 'clear')
+        enviroment()
         print
         print('====================================')
         print('#         All The Scans            #')
@@ -128,66 +135,32 @@ while ans:
         targetall = input("Enter the target URL : ")
         outputall = input("Enter the output folder : ")
         ipall = socket.gethostbyname(targetall)
-        print("___________________________________________________________________________")
+        underscore()
         print
         os.system('gnome-terminal -- bash -c "nmap -A '+ipall+' -o '+outputall+'/nmap.txt && bash"')
         os.system('gnome-terminal -- bash -c "python3 /opt/dirsearch/dirsearch.py -u '+targetall+ ' -e * --simple-report='+outputall+'/dirsearch.txt && bash"')
         os.system('gnome-terminal -- bash -c "nikto +h '+targetall+' -output '+outputall+'/nikto.txt && bash"')
         if ans=="":
-          os.system('cls' if os.name == 'nt' else 'clear')
-    
+          enviroment()
+
     elif ans=="G":
-      os.system('cls' if os.name == 'nt' else 'clear')
+        enviroment()
 
-      ans=True
-      while ans:
-
-          print
-          print('====================================')
-          print('#         Get The Tools            #')
-          print('====================================')
-          print("1. Download Nmap")
-          print("2. Download Dirsearch")
-          print("3. Download Nikto")
-          print("M. Main Menu")
-          print('====================================')
-          print 
-          ans=input("Which program do you still need? Enter the number: ")
-
-          if ans=="1":
-              print('Downloading Nmap Now...')
-              print
-              print
-              os.system("sudo apt-get install nmap")
-
-          elif ans=="2":
-              print('Downloading Dirsearch Now...')
-              print
-              print
-              os.system("cd /opt && sudo git clone https://github.com/maurosoria/dirsearch.git")
-
-          elif ans=="3":
-              print('Downloading Nikto Now...')
-              print
-              print
-              os.system("sudo apt-get install nikto")
-
-          elif ans=="M":
-              os.system('cls' if os.name == 'nt' else 'clear')
-              os.system('python3 webmap.py')
-          else:
-                os.system('cls' if os.name == 'nt' else 'clear')
-                print
-                print("Not Valid Choice Try again")
-                print
-                os.system("python3 webmap.py")
-                ans = None
+        ans=True
+        while ans:
+            print("Downloading all tools...")
+            os.system("sudo apt-get install nmap -y")
+            os.system("cd /opt && sudo git clone https://github.com/maurosoria/dirsearch.git")
+            os.system("sudo apt-get install nikto -y")
+            enviroment()
+            ans=None
     elif ans=="E":
-        os.system('cls' if os.name == 'nt' else 'clear')
+        enviroment()
         ans = None
     else:
-    	os.system('cls' if os.name == 'nt' else 'clear')
-    	print
-    	print("Not Valid Choice Try again")
-    	print
-    	os.system("python3 webmap.py")
+        enviroment()
+        print
+        print("Not Valid Choice Try again")
+        print
+        os.system("python3 webmap.py")
+        ans = None
